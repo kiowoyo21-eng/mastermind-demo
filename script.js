56c95bd5c44baa21f57b6c1872f4edf44c873197
kiowoyo21-eng <kiowoyo21@gmail.com>
2026-09-20 11:30:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-const toggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.primary-nav');
 const navLinks = [...document.querySelectorAll('.nav-link[href^="#"]')];
 
-toggle?.addEventListener('click', () => {
-  const open = nav.classList.toggle('open');
-  toggle.setAttribute('aria-expanded', String(open));
-});
-
-navLinks.forEach(link => link.addEventListener('click', () => {
-  nav.classList.remove('open');
-  toggle?.setAttribute('aria-expanded', 'false');
-}));
 
 const sections = navLinks
   .map(link => document.querySelector(link.getAttribute('href')))
